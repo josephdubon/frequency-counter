2ca8a688172d1a754524ae8573156e4abe7dd7bc
Joseph Dubon <josephdubon@pm.me>
2020-06-16 14:34:06 -0700

    Assessment complete.

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
         let letterCounts = {}
-        let wordCounts = {}
 
         document.getElementById("countButton").onclick = function () {
             let typedText = document.getElementById("textInput").value
-            // This changes all the letter to lower case.  
             typedText = typedText.toLowerCase()
-            // This gets rid of all the characters except letters, spaces, and apostrophes. 
             typedText = typedText.replace(/[^a-z'\s]+/g, "")
 
             // Letter Counts
             for (let i = 0; i < typedText.length; i++) {
-                currentLetter = typedText[i]
+                let currentLetter = typedText[i]
 
                 if (letterCounts[currentLetter] === undefined) {
                     letterCounts[currentLetter] = 1
@@ -26,23 +23,22 @@
                 document.getElementById("lettersDiv").appendChild(span)
             }
 
-            // Words Count
-            const words = typedText.split(/\s/);
+            // Word Counts
 
-            for (let k = 0; k < typedText.length; k++) {
-                currentWord = typedText[k]
-
+            const words = typedText.split(" ")
+            const wordCounts = {}
+            for (let k = 0; k < words.length; k++) {
+                let currentWord = words[k]
                 if (wordCounts[currentWord] === undefined) {
                     wordCounts[currentWord] = 1
                 } else {
                     wordCounts[currentWord]++
                 }
             }
-
-            for (let words in wordCounts) {
+            for(let text in wordCounts) {
                 const span = document.createElement("span")
-                const wordsContent = document.createTextNode('"' + words + "\": " + wordCounts[words] + ", ")
-                span.appendChild(wordsContent)
+                const textContent = document.createTextNode('"' + text + "\": " + wordCounts[text] + ",")
+                span.appendChild(textContent)
                 document.getElementById("wordsDiv").appendChild(span)
             }
         }
