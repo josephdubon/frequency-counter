6a3e95eb9dfc0d0879b7d27ecd8b4ecb29288465
Joseph Dubon <josephdubon@pm.me>
2020-06-16 11:49:01 -0700

    Corrected typo on index.html.

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-// Count button click handler
-document.getElementById("countButton").onclick = function name() {
-    // code here
+document.getElementById("countButton").onclick = function () {
+    let typedText = document.getElementById("textInput").value;
+    typedText = typedText.toLowerCase();
+    // This changes all the letter to lower case.  
+    typedText = typedText.replace(/[^a-z'\s]+/g, "");
+    // This gets rid of all the characters except letters, spaces, and apostrophes.  
+    function letters(typedText) {
+        let letterCounts = {};
+        for (let i = 0; i < typedText.length; i++) {
+            currentLetter = typedText[i];
+            if (letterCounts[currentLetter] === undefined) {
+                letterCounts[currentLetter] = 1;
+            } else {
+                letterCounts[currentLetter]++;
+            }
+            for (let letter in letterCounts) {
+                const span = document.createElement("span");
+                const textContent = document.createTextNode('"' + letter + "\": " + letterCounts[letter] + ", ");
+                span.appendChild(textContent);
+                document.getElementById("lettersDiv").appendChild(span);
+            }
+        }
+    }
+
+    function words(typedText) {
+        words = typedText.split(/\s/);
+        let wordCounts = {};
+        for (let i = 0; i < typedText.length; i++) {
+            currentWord = typedText[i];
+            if (wordCounts[currentWord] === undefined) {
+                wordCounts[currentWord] = 1;
+            } else {
+                wordCounts[currentWord]++;
+            }
+            for (let word in wordCounts) {
+                const span = document.createElement("span");
+                const textContent = document.createTextNode('"' + word + "\": " + wordCounts[word] + ", ");
+                span.appendChild(textContent);
+                document.getElementById("wordsDiv").appendChild(span);
+            }
+        }
+    }
 }
-
-// Text field input reference
-let typedText = document.getElementById("textInput").value
-
-// Ignore capitalization and punctuation
-
-// Change all the letter to lower case
-typedText = typedText.toLowerCase()
-
-// Get rid of all the characters except letters, spaces, and apostrophes
-typedText = typedText.replace(/[^a-z'\s]+/g, "")
-
-// Loop over all characters in typedText 
-for (let i = 0; i < typedText.length; i++) {
-    currentLetter = typedText[i];
-    // do something for each letter
-}
-
-// Keep counts of the number of times each different letter is used in the text
-let letterCounts = {}
-
-// Initialize new letter count to 1; otherwise add one to the count
-if (letterCounts[currentLetter] === undefined) {
-    letterCounts[currentLetter] = 1
-} else {
-    letterCounts[currentLetter]++
-}
-
-// Output how many times each letter occurred
-for (let letter in letterCounts) {
-    const span = document.createElement("span")
-    const textContent = document.createTextNode("''" + letter + "\":  " + letterCounts[letter] + ", ")
-    span.appendChild(textContent)
-    document.getElementById("lettersDiv").appendChild(span)
-}
-
-// Break the input string into words by splitting it on the spaces
-words = typedText.split(/\s/)
