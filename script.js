ad7d1a2279090a41110f54f32f6545b1197c54ed
Joseph Dubon <josephdubon@pm.me>
2020-06-16 12:12:35 -0700

    Formatted code.

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-document.getElementById("countButton").onclick = function () {
-    let typedText = document.getElementById("textInput").value;
-    typedText = typedText.toLowerCase();
-    // This changes all the letter to lower case.  
-    typedText = typedText.replace(/[^a-z'\s]+/g, "");
-    // This gets rid of all the characters except letters, spaces, and apostrophes.  
-    function letters(typedText) {
-        let letterCounts = {};
-        for (let i = 0; i < typedText.length; i++) {
-            currentLetter = typedText[i];
-            if (letterCounts[currentLetter] === undefined) {
-                letterCounts[currentLetter] = 1;
-            } else {
-                letterCounts[currentLetter]++;
+        let letterCounts = {}
+
+        document.getElementById("countButton").onclick = function () {
+            let typedText = document.getElementById("textInput").value
+            typedText = typedText.toLowerCase()
+            // This changes all the letter to lower case.  
+            typedText = typedText.replace(/[^a-z'\s]+/g, "")
+            // This gets rid of all the characters except letters, spaces, and apostrophes.  
+            for (let i = 0; i < typedText.length; i++) {
+                currentLetter = typedText[i]
+
+                if (letterCounts[currentLetter] === undefined) {
+                    letterCounts[currentLetter] = 1
+                } else {
+                    letterCounts[currentLetter]++
+                }
             }
             for (let letter in letterCounts) {
-                const span = document.createElement("span");
-                const textContent = document.createTextNode('"' + letter + "\": " + letterCounts[letter] + ", ");
-                span.appendChild(textContent);
-                document.getElementById("lettersDiv").appendChild(span);
+                const span = document.createElement("span")
+                const textContent = document.createTextNode('"' + letter + "\": " + letterCounts[letter] + ", ")
+                span.appendChild(textContent)
+                document.getElementById("lettersDiv").appendChild(span)
             }
-        }
-    }
 
-    function words(typedText) {
-        words = typedText.split(/\s/);
-        let wordCounts = {};
-        for (let i = 0; i < typedText.length; i++) {
-            currentWord = typedText[i];
-            if (wordCounts[currentWord] === undefined) {
-                wordCounts[currentWord] = 1;
-            } else {
-                wordCounts[currentWord]++;
+            const words = typedText.split(" ")
+            let wordCounts = {}
+
+            for (let k = 0; k < typedText.length; k++) {
+                currentWord = typedText[k]
+                if (wordCounts[currentWord] === undefined) {
+                    wordCounts[currentWord] = 1
+                } else {
+                    wordCounts[currentWord]++
+                }
             }
             for (let word in wordCounts) {
-                const span = document.createElement("span");
-                const textContent = document.createTextNode('"' + word + "\": " + wordCounts[word] + ", ");
-                span.appendChild(textContent);
-                document.getElementById("wordsDiv").appendChild(span);
+                const span = document.createElement("span")
+                const textContent = document.createTextNode('"' + word + "\": " + wordCounts[word] + ", ")
+                span.appendChild(textContent)
+                document.getElementById("wordsDiv").appendChild(span)
             }
         }
-    }
-}
